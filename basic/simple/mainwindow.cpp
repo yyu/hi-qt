12ec0c0a734c5ed5c109e4b546deb7be4913ae0a
y² <yy@yuyuan.org>
2018-12-06 20:43:33 -0800

    added "Click Me To Quit" button

--- basic/simple/mainwindow.cpp
+++ basic/simple/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->clickToQuitButton, &QPushButton::clicked, QApplication::instance(), &QApplication::quit);
 }
 
 MainWindow::~MainWindow()
